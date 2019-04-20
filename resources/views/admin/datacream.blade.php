5a72b0fb77415950940f5ad954bbaad97fccd41a
mubarok96 <rifkimubarok96@gmail.com>
2019-04-20 10:21:44 +0700

    bebas2

--- resources/views/admin/datacream.blade.php
+++ resources/views/admin/datacream.blade.php
@@ -20,13 +20,13 @@
       <div class="widget-box">
         <div class="widget-title bg_lg">
           <span class="icon"><i class="icon-plus"></i></span>
-          <h5>Tambah Jenis Kulit</h5>
+          <h5>Tambah Cream</h5>
         </div>
         
         <div class="widget-content">
           <div class="controls controls-row">
-            <label class="span3 m-wrap"><b>Kode Jenis Kulit</b></label>
-            <label class="span3 m-wrap"><b>Nama Jenis Kulit</b></label>
+            <label class="span3 m-wrap"><b>Kode Cream</b></label>
+            <label class="span3 m-wrap"><b>Nama Cream</b></label>
           </div>
           <!-- form action -->
           <form action="{{ url('/simpanjeniskulit') }}" method="post">
@@ -58,8 +58,8 @@
               <thead>
                 <tr>
                   <th style="text-align: left; font-size: 13px">No</th>
-                  <th style="text-align: left; font-size: 13px">Kode Jenis Kulit</th>
-                  <th style="text-align: left; font-size: 13px">Nama Jenis Kulit</th>
+                  <th style="text-align: left; font-size: 13px">Kode Cream</th>
+                  <th style="text-align: left; font-size: 13px">Nama Cream</th>
                   <th style="text-align: left; font-size: 13px">Aksi</th>
                 </tr>
               </thead>
